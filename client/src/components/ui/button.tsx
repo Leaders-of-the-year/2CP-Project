c33251e474ce772fde1f4d5a92e9f197341e8a63
Aymen-drid <aymnehocam@gmail.com>
2025-02-03 16:00:41 +0100

    finish button bug

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -25,6 +25,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
+        custom:" w-[200px] h-[52px] px-4 py-6"
       },
     },
     defaultVariants: {
