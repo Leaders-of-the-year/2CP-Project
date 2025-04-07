8910360534f71d942ec1c63e4d06accd6f2d0aba
Aymen-drid <aymnehocam@gmail.com>
2025-04-07 22:42:51 +0100

    video call up

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
 import VideoCall from "@/app/features/videocall/videocall";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-main text-white">
-    <VideoCall />
-  </main>
-
-  );
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white p-4">
+      <VideoCall role="doctor" />
+    </main>
+  )
 }
+
