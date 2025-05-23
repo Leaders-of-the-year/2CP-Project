8dd891904c89eb2b19fc9ab78cdcf14033642005
Aymen-drid <aymnehocam@gmail.com>
2025-05-23 19:39:46 +0100

    add new info

--- client/config.ts
+++ client/config.ts
@@ -3,5 +3,5 @@ export const SERVER_IP = process.env.SERVER_IP || '192.168.98.215';
 export const CLIENT_IP = process.env.CLIENT_IP || '192.168.98.215';
 export const CLIENT_PORT = process.env.CLIENT_PORT || '3000';
 export const SERVER_PORT = process.env.SERVER_PORT || '3001';
-export const SERVER_URL = `https://default-server-420e.onrender.com`;
+export const SERVER_URL = `https://default-server-production.up.railway.app`;
 export const CLIENT_URL = `https://${CLIENT_IP}:${CLIENT_PORT}`;
