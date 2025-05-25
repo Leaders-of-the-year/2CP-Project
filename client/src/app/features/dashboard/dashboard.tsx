88aab45477c516eb5c2edebcdc49b67b3596f2c8
Aymen-drid <aymnehocam@gmail.com>
2025-05-25 01:32:46 +0100

    add new

--- client/src/app/features/dashboard/dashboard.tsx
+++ client/src/app/features/dashboard/dashboard.tsx
@@ -140,7 +140,7 @@ console.log("Weight:", weight);
           <PatientRecords
             records={userData.records.map((record: any) => ({
               type: record.type as "blood" | "heart" | "xray",
-              date: record.date,s
+              date: record.date
             }))}
           />
           <Prescriptions prescriptions={userData.prescriptions} />
